bbe825d20d4e52dbb8e2405c618bd960fdb4b8a7
Ifeadigo Udeh <ifeadigod69@gmail.com>
2022-08-07 20:58:44 +0100

    project

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from pathlib import Path
+import django_heroku
+import dj_database_url
 from dotenv import load_dotenv,find_dotenv
 
 load_dotenv(find_dotenv())
@@ -129,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+django_heroku.settings(locals())
